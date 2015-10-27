e937f5af20e127c5fc0cdeeadc49f94ee627ee71
MateuszPu <mat.pulka@gmail.com>
2015-10-27 20:48:24 +0100

    Refactoring: removed one stock ticker not quoted on gpw, and decrease size of stock of details

--- src/main/java/com/gpw/radar/domain/enumeration/StockTicker.java
+++ src/main/java/com/gpw/radar/domain/enumeration/StockTicker.java
@@ -7,7 +7,7 @@ public enum StockTicker implements Ticker {
 	abc, abe, acp, act, ago, alc, ali, alr, amc, aml, apt, arr, asb, ase, ast, atc,
 	atd, att, bbd, bdx, bft, bhw, bio, bmc, brs, bsc, btm, bzw, car, ccc, cdr,
 	cez, chs, cie, cig, cmp, cmr, col, cps, crm, czt, dbc, dom, dud, duo, eat, ech,
-	eex, egs, elb, emp, emt, ena, eng, erb, erg, eur, fmf, fro, fsg, fte, gch, gcn,
+	eex, egs, elb, emp, emt, ena, eng, erb, erg, eur, fmf, fro, fsg, fte, gcn,
 	gnb, gpw, gri, grj, gtc, gtn, hrs, hwe, idg, inc, ind, ing, ink, ipl, ipx, itg,
 	jsw, jwc, kan, ker, kgh, kom, kpx, kri, krs, kru, ksw, kty, lbt, lbw, lcc, len,
 	lpp, lts, ltx, lwb, mab, mag, mbk, mci, mcr, mdg, mds, mil, mlk, mol, mon, msw,
