717eb4f633b41e9f904d34031dea2966c7437e24
MateuszPu <mat.pulka@gmail.com>
2015-11-19 21:50:34 +0100

    Refactored junit test for stockDetailsTextFileParser

--- src/test/java/com/gpw/radar/service/StockDetailsTextFileParserServiceTest.java
+++ src/test/java/com/gpw/radar/service/StockDetailsTextFileParserServiceTest.java
@@ -22,7 +22,7 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.StrictAssertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
@@ -52,41 +52,41 @@ public class StockDetailsTextFileParserServiceTest {
     @Test
     public void stockDetailsSize() {
         List<StockDetails> list = stockDetailsTextFileParserService.parseStockDetailsByStockFromTxtFile(stock, inputStreamOfStockDetails);
-        assertTrue("size of the content should be 20", list.size() == 20);
+        assertThat(list.size()).isEqualTo(20);
     }
 
     @Test
     public void stockDetailsContent() {
         List<StockDetails> list = stockDetailsTextFileParserService.parseStockDetailsByStockFromTxtFile(stock, inputStreamOfStockDetails);
         StockDetails firstStockDetails = list.get(0);
-        assertTrue("First element in list should have volume = 277939", firstStockDetails.getVolume().equals(277939l));
-        assertTrue("First element in list should have open price = 17.9", firstStockDetails.getOpenPrice().equals(new BigDecimal("17.9")));
-        assertTrue("First element in list should have max price = 18.38", firstStockDetails.getMaxPrice().equals(new BigDecimal("18.38")));
-        assertTrue("First element in list should have min price = 17.81", firstStockDetails.getMinPrice().equals(new BigDecimal("17.81")));
-        assertTrue("First element in list should have close price = 17.87", firstStockDetails.getClosePrice().equals(new BigDecimal("17.87")));
-        assertTrue("First element in list should have date = 2014-12-03", firstStockDetails.getDate().equals(LocalDate.of(2014,12,03)));
+        assertThat(firstStockDetails.getVolume()).isEqualTo(277939);
+        assertThat(firstStockDetails.getOpenPrice()).isEqualTo(new BigDecimal("17.9"));
+        assertThat(firstStockDetails.getMaxPrice()).isEqualTo(new BigDecimal("18.38"));
+        assertThat(firstStockDetails.getMinPrice()).isEqualTo(new BigDecimal("17.81"));
+        assertThat(firstStockDetails.getClosePrice()).isEqualTo(new BigDecimal("17.87"));
+        assertThat(firstStockDetails.getDate()).isEqualTo(LocalDate.of(2014,12,03));
     }
 
     @Test
     public void stockFiveMinutesDetailsSize() {
         List<StockFiveMinutesDetails> list = stockDetailsTextFileParserService.parseStockFiveMinutesDetailsByStockFromTxtFile(stock, inputStreamOfStockFiveMinutesDetails);
-        assertTrue("size of the content should be 85", list.size() == 85);
+        assertThat(list.size()).isEqualTo(85);
     }
 
     @Test
     public void stockFiveMinutesDetailsContent() {
         List<StockFiveMinutesDetails> list = stockDetailsTextFileParserService.parseStockFiveMinutesDetailsByStockFromTxtFile(stock, inputStreamOfStockFiveMinutesDetails);
         StockFiveMinutesDetails stockFiveMinutesDetails = list.get(0);
-        assertTrue("First element in list should have volume=1417", stockFiveMinutesDetails.getVolume() == 1417);
-        assertTrue("First element in list should have time of 9:05", stockFiveMinutesDetails.getTime().equals(LocalTime.of(9,5)));
-        assertTrue("First element in list should have date of 2015-11-10 9:05", stockFiveMinutesDetails.getDate().equals(LocalDateTime.of(2015,11,10,9,5)));
+        assertThat(stockFiveMinutesDetails.getVolume()).isEqualTo(1417);
+        assertThat(stockFiveMinutesDetails.getTime()).isEqualTo(LocalTime.of(9,5));
+        assertThat(stockFiveMinutesDetails.getDate()).isEqualTo(LocalDateTime.of(2015,11,10,9,5));
     }
 
     @Test
     public void stockFiveMinutesDetailsFillEmptyRangeSize() {
         List<StockFiveMinutesDetails> list = stockDetailsTextFileParserService.parseStockFiveMinutesDetailsByStockFromTxtFile(stock, inputStreamOfStockFiveMinutesDetails);
         List<StockFiveMinutesDetails> filledList = stockDetailsTextFileParserService.fillEmptyTimeAndCumulativeVolume(list);
-        assertTrue("size of the content should be 94", filledList.size() == 94);
+        assertThat(filledList.size()).isEqualTo(94);
     }
 
     @Test
@@ -95,8 +95,8 @@ public class StockDetailsTextFileParserServiceTest {
         List<StockFiveMinutesDetails> filledList = stockDetailsTextFileParserService.fillEmptyTimeAndCumulativeVolume(list);
         StockFiveMinutesDetails firstStockFiveMinutesDetails = filledList.get(0);
         StockFiveMinutesDetails thirdStockFiveMinutesDetails = filledList.get(2);
-        assertTrue("First element in filled list should have cumulated volume = 1417", firstStockFiveMinutesDetails.getCumulatedVolume() == 1417);
-        assertTrue("Third element in filled list should have volume = 0", thirdStockFiveMinutesDetails.getVolume() == 0);
-        assertTrue("Third element in filled list should have time = 9:15", thirdStockFiveMinutesDetails.getTime().equals(LocalTime.of(9,15)));
+        assertThat(firstStockFiveMinutesDetails.getCumulatedVolume()).isEqualTo(1417);
+        assertThat(thirdStockFiveMinutesDetails.getVolume()).isEqualTo(0);
+        assertThat(thirdStockFiveMinutesDetails.getTime()).isEqualTo(LocalTime.of(9,15));
     }
 }
