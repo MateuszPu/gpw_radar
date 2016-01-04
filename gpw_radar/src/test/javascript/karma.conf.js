1d6da7f0f65b9d104da8dfb5d78244426b043b28
MateuszPu <mat.pulka@gmail.com>
2016-01-04 18:04:29 +0100

    init

--- gpw_radar/src/test/javascript/karma.conf.js
+++ gpw_radar/src/test/javascript/karma.conf.js
@@ -43,7 +43,6 @@ module.exports = function (config) {
             'main/webapp/bower_components/ng-file-upload/ng-file-upload.js',
             'main/webapp/bower_components/ngInfiniteScroll/build/ng-infinite-scroll.js',
             'main/webapp/bower_components/PACE/pace.js',
-            'main/webapp/bower_components/slimScroll/jquery.slimscroll.min.js',
             'main/webapp/bower_components/sockjs-client/dist/sockjs.js',
             'main/webapp/bower_components/stomp-websocket/lib/stomp.min.js',
             'main/webapp/bower_components/angular-mocks/angular-mocks.js',
---
+++ technical_analysis_indicators/src/main/java/technical/analysis/indicators/App.java
@@ -0,0 +1,17 @@
+package technical.analysis.indicators;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+        	System.out.println("ad");
+        
+        
+    }
+    
+}
