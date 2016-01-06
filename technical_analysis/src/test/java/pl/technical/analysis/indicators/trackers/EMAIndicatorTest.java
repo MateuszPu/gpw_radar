96a87f165dbf6665b97d30e316f0106661c10691
MateuszPu <mat.pulka@gmail.com>
2016-01-06 19:49:01 +0100

    Added WMA indicator

--- technical_analysis/src/test/java/pl/technical/analysis/indicators/trackers/EMAIndicatorTest.java
+++ technical_analysis/src/test/java/pl/technical/analysis/indicators/trackers/EMAIndicatorTest.java
@@ -10,17 +10,16 @@ import static org.assertj.core.api.StrictAssertions.assertThat;
 
 public class EMAIndicatorTest {
     private List<Tickable> tickers = ResourcesGetter.getTickers("kgh.txt");
-    EMAIndicator EMA;
 
     @Test
     public void SMA15Test() {
-        EMA = new EMAIndicator(tickers, 15);
+        EMAIndicator EMA = new EMAIndicator(tickers, 15);
         assertThat(EMA.getLast()).isEqualTo(63.44);
-}
+    }
 
     @Test
     public void SMA30Test() {
-        EMA = new EMAIndicator(tickers, 30);
+        EMAIndicator EMA = new EMAIndicator(tickers, 30);
         assertThat(EMA.getLast()).isEqualTo(67.48);
     }
 }
--- technical_analysis/src/test/java/pl/technical/analysis/indicators/trackers/WMAIndicatorTest.java
+++ technical_analysis/src/test/java/pl/technical/analysis/indicators/trackers/WMAIndicatorTest.java
@@ -8,20 +8,18 @@ import java.util.List;
 
 import static org.assertj.core.api.StrictAssertions.assertThat;
 
-
-public class SMAIndicatorTest {
-
+public class WMAIndicatorTest {
     private List<Tickable> tickers = ResourcesGetter.getTickers("kgh.txt");
 
     @Test
-    public void SMA15Test() {
-        SMAIndicator SMA = new SMAIndicator(tickers, 15);
-        assertThat(SMA.getLast()).isEqualTo(61.2);
+    public void WMA15Test() {
+        WMAIndicator WMA = new WMAIndicator(tickers, 15);
+        assertThat(WMA.getLast()).isEqualTo(61.96);
     }
 
     @Test
-    public void SMA30Test() {
-        SMAIndicator SMA = new SMAIndicator(tickers, 30);
-        assertThat(SMA.getLast()).isEqualTo(66.93);
+    public void WMA30Test() {
+        WMAIndicator WMA = new WMAIndicator(tickers, 30);
+        assertThat(WMA.getLast()).isEqualTo(63.81);
     }
 }
