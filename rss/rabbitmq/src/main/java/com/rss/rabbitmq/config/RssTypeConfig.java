d9fd6ad5eb135e5c432d8629738b305ff89518d6
Mateusz <mat.pulka@gmail.com>
2016-08-21 09:52:20 +0200

    clean-up

--- rss/rabbitmq/src/main/java/com/rss/rabbitmq/config/RssTypeConfig.java
+++ rss/rabbitmq/src/main/java/com/rss/rabbitmq/config/RssTypeConfig.java
@@ -1,8 +1,6 @@
 package com.rss.rabbitmq.config;
 
 import com.rss.parser.Parser;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
