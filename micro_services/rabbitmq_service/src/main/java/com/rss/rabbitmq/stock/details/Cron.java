4dafa7170ca7944c548415420a85f0f647f854ec
Mateusz <mat.pulka@gmail.com>
2016-11-23 18:47:45 +0100

    Added calculation of stock indicator after update stock details

--- micro_services/rabbitmq_service/src/main/java/com/rss/rabbitmq/stock/details/Cron.java
+++ micro_services/rabbitmq_service/src/main/java/com/rss/rabbitmq/stock/details/Cron.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service("stockDetailsCron")
 public class Cron {
