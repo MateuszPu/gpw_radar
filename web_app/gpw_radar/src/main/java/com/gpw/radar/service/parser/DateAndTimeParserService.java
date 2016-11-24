45ea0aa0a93b2a2e36214c46267590e5eb5f9523
Mateusz <mat.pulka@gmail.com>
2016-11-24 17:59:43 +0100

    Fix

--- web_app/gpw_radar/src/main/java/com/gpw/radar/service/parser/DateAndTimeParserService.java
+++ web_app/gpw_radar/src/main/java/com/gpw/radar/service/parser/DateAndTimeParserService.java
@@ -4,18 +4,14 @@ import com.gpw.radar.service.parser.web.UrlStreamsGetterService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
-import javax.inject.Inject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
 import java.util.Map;
 
 @Service
