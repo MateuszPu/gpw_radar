eb80e896c9ba903ee005d29845560564959e5ab5
Mateusz <mat.pulka@gmail.com>
2016-12-17 11:13:02 +0100

    Added verification of rss link

--- build_project.sh
+++ build_project.sh
@@ -11,9 +11,9 @@ mkdir target
 cd web_app/gpw_radar
 mvn -Pprod clean package
 cp -p $(find target/ -name 'gpw*.war') ../../target
-cd ../../micro_services/rabbitmq_service
+cd ../../micro_services
 mvn clean package
-cp -p $(find target/ -name 'rabbitmq*.jar') ../../target
+cp -p $(find rabbitmq_service/target/ -name 'rabbitmq*.jar') ../target
 cd ../../
 perl -pi -e 's/127.0.0.1/192.168.99.100/g' configs/rabbitmq/properties/rabbitmq_config.properties
 perl -pi -e 's/'$1'/mail_password/g' web_app/gpw_radar/src/main/resources/config/application.yml
