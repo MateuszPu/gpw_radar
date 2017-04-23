34569f2a89840cfe32a16eec94e173fcdb7c1001
Mateusz <mat.pulka@gmail.com>
2017-04-23 18:18:12 +0200

    Removed unused imports

--- web_app/gpw_radar/src/main/java/com/gpw/radar/rabbitmq/consumer/rss/news/MessageTransformer.java
+++ web_app/gpw_radar/src/main/java/com/gpw/radar/rabbitmq/consumer/rss/news/MessageTransformer.java
@@ -11,9 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.EnumSet;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.regex.Matcher;
