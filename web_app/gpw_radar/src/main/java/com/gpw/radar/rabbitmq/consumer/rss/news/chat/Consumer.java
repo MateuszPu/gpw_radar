182b849bd09b7d1568b08dd31f1bb6fdff47d44e
Mateusz <mat.pulka@gmail.com>
2017-04-30 09:07:44 +0200

    Removed unused import

--- web_app/gpw_radar/src/main/java/com/gpw/radar/rabbitmq/consumer/rss/news/chat/Consumer.java
+++ web_app/gpw_radar/src/main/java/com/gpw/radar/rabbitmq/consumer/rss/news/chat/Consumer.java
@@ -6,8 +6,6 @@ import com.gpw.radar.domain.chat.ChatMessage;
 import com.gpw.radar.rabbitmq.consumer.rss.news.MessageTransformer;
 import com.gpw.radar.repository.chat.ChatMessageRepository;
 import com.gpw.radar.service.sockets.SocketMessageService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
